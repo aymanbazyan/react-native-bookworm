400e7b6961ccf27a3699c6a0073c4d528cd44888
aymanbazyan <aymanbazyan@gmail.com>
2025-07-16 10:26:57 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
+import job from "./lib/cron.js";
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+job.start();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
